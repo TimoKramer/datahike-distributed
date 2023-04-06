c718f00c292fbbb373d793e6227a8cedb78ba75a
Christian Weilbach <ch_weil@topiq.es>
2023-04-06 09:51:49 -0700

    Update example to new datahike-server.

--- src/datahike_distributed/core.clj
+++ src/datahike_distributed/core.clj
@@ -6,28 +6,32 @@
 (log/set-level! :trace)
 
 (def distributed-cfg1 {:store {:backend :file
-                               :path "/tmp/dh-file"}
-                       :name "users"
+                               :scope "test.lambdaforge.net"
+                               :path "/tmp/users"}
                        :keep-history? true
                        :schema-flexibility :write
                        :writer {:backend :datahike-server
                                 :client-config  {:timeout 300
-                                                 :endpoint "http://localhost:4444"
-                                                 :db-name "users"}}})
+                                                 :endpoint "http://localhost:3333"}}})
+
+(comment
+  ;; not supported client side yet
+  (d/create-database distributed-cfg1)
+  (d/delete-database distributed-cfg1))
 
 (def conn (d/connect distributed-cfg1))
 
-(d/transact conn [[:db/add -1 :name "bar"]])
-(d/transact conn [[:db/add -1 :name "foo"]])
-
-(d/transact conn [{:db/ident :name
-                   :db/valueType :db.type/string
+(d/transact conn [{:db/ident       :name
+                   :db/valueType   :db.type/string
                    :db/cardinality :db.cardinality/one}
-                  {:db/ident :age
-                   :db/valueType :db.type/long
+                  {:db/ident       :age
+                   :db/valueType   :db.type/long
                    :db/cardinality :db.cardinality/one}])
+
+(d/transact conn [[:db/add -1 :name "foo"]])
 
 (d/q {:query '[:find ?name
                :where [_ :name ?name]]}
-     @conn) 
+     @conn)
 
+(d/release conn)
